6feac60888a41dcd2f33564c68281d7a0e803771
Priyanka Ganesan <gpriyankapms@gmail.com>
2024-07-02 13:22:45 -0500

    Final

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore, collection } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyB--s3CQjnFTMBWGN-Yp-z4QqyMeZzLKQI",
+  authDomain: "sticky-note-8c32b.firebaseapp.com",
+  projectId: "sticky-note-8c32b",
+  storageBucket: "sticky-note-8c32b.appspot.com",
+  messagingSenderId: "581285297344",
+  appId: "1:581285297344:web:6d3e28952e13fce9f78125",
 };
 
 // Initialize Firebase
